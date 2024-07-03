5990c1ab0d1be56bb1a60dd08ad87c8429527101
Jun Hao <yan46648@gmail.com>
2024-07-03 19:07:14 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -12,37 +12,12 @@ function App() {
   const [torchToggled, setTorchToggled] = useState(false);
 
   useEffect(() => {
-    const getDevices = async () => {
-      try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter(
-          (device) => device.kind === "videoinput"
-        );
-        setDevices(videoDevices);
-      } catch (error) {
-        console.error("Error enumerating devices:", error);
-      }
-    };
-    getDevices();
+    (async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter((i) => i.kind === "videoinput");
+      setDevices(videoDevices);
+    })();
   }, []);
-
-  const takeHighResolutionPhoto = async () => {
-    try {
-      if (cameraRef.current) {
-        const capabilities = await cameraRef.current.getCameraCapabilities();
-        const maxResolution = capabilities.reduce((prev, current) =>
-          prev.width * prev.height > current.width * current.height
-            ? prev
-            : current
-        );
-        const { width, height } = maxResolution;
-        const photo = await cameraRef.current.takePhoto({ width, height });
-        setImage(photo);
-      }
-    } catch (error) {
-      console.error("Error capturing photo:", error);
-    }
-  };
 
   return (
     <div className="wrapper">
@@ -59,7 +34,7 @@ function App() {
           ref={cameraRef}
           aspectRatio="cover"
           facingMode="environment"
-          numberOfCamerasCallback={(count) => setNumberOfCameras(count)}
+          numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
           videoSourceDeviceId={activeDeviceId}
           errorMessages={{
             noCameraAccessible:
@@ -67,7 +42,7 @@ function App() {
             permissionDenied:
               "Permission denied. Please refresh and give camera permission.",
             switchCamera:
-              "It is not possible to switch camera to a different one because there is only one video device accessible.",
+              "It is not possible to switch camera to different one because there is only one video device accessible.",
             canvas: "Canvas is not supported.",
           }}
           videoReadyCallback={() => {
@@ -81,9 +56,9 @@ function App() {
             setActiveDeviceId(event.target.value);
           }}
         >
-          {devices.map((device) => (
-            <option key={device.deviceId} value={device.deviceId}>
-              {device.label}
+          {devices.map((d) => (
+            <option key={d.deviceId} value={d.deviceId}>
+              {d.label}
             </option>
           ))}
         </select>
@@ -96,10 +71,15 @@ function App() {
         />
         <button
           className="button take-photo-button"
-          onClick={takeHighResolutionPhoto}
-        >
-          Take Photo
-        </button>
+          onClick={() => {
+            if (cameraRef.current) {
+              const photo = cameraRef.current.takePhoto({
+                quality: 1,
+              });
+              setImage(photo);
+            }
+          }}
+        />
         {torchToggled && (
           <button
             className="button torch-button toggled"
@@ -108,9 +88,7 @@ function App() {
                 setTorchToggled(cameraRef.current.toggleTorch());
               }
             }}
-          >
-            Toggle Torch
-          </button>
+          />
         )}
         <button
           className="button change-facing-camera-button"
@@ -120,9 +98,7 @@ function App() {
               cameraRef.current.switchCamera();
             }
           }}
-        >
-          Switch Camera
-        </button>
+        />
       </div>
     </div>
   );
